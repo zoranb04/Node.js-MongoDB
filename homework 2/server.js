b8868d306fcd0cb71ed8a617b6002adab5f3e76e
zoranb04 <bush_zoki@hotmail.com>
2021-11-30 23:58:27 +0100

    homework

--- homework 2/server.js
+++ homework 2/server.js
@@ -1,9 +1,9 @@
+require('dotenv').config();
 const http = require("http");
 const app = require("./app");
 
-const server = http.createServer(app);
-
 const PORT = process.env.PORT;
+const server = http.createServer(app);
 
 server.listen(PORT, () => {
     console.log("Zdravo!");
